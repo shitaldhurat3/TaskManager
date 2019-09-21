76798bc9d82321b8d6f169a4dc9200b2987cee02
shitaldhurat3 <shital.d@testyantra.in>
2019-09-21 23:45:02 +0530

    TaskManager Project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { FormsModule }  from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { AddTaskComponent } from './add-task/add-task.component';
+import { ViewAllTaskComponent } from './view-all-task/view-all-task.component';
+import { HeadComponent } from './head/head.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    AddTaskComponent,
+    ViewAllTaskComponent,
+    HeadComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot([
+      { path: 'addTask', component: AddTaskComponent},
+      { path: 'viewAll', component: ViewAllTaskComponent}
+
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
